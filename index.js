ebf94a3a317c0201bfd5540741372a9d703c3637
preciousnjukia <preciousnjukia@gmail.com>
2023-07-06 14:49:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+let num1 = 2
+let num2 = 31
+const multiply = num1 * num2;
+
+let num3 = 8
+let num4 = 2
+const mod = num3 / num4;
+
+let numbers = [8, 20, 13, 10]
+const max = Math.max(...numbers);
+
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
